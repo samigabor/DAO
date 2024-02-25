4bc6cc2a47e1d9c54ce03c0fc2eb29bd478fded0
samigabor <samoila.gabor@yahoo.com>
2024-02-25 21:02:48 +0200

    feat: add minting functionality for testing purposes

--- src/GovToken.sol
+++ src/GovToken.sol
@@ -14,6 +14,11 @@ contract GovToken is ERC20, ERC20Permit, ERC20Votes, Ownable {
         Ownable(msg.sender)
     {}
 
+    /// DANGER: only for testing. Anyone can mint tokens!!!
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+
     // The following functions are overrides required by Solidity.
 
     function _update(address from, address to, uint256 value)
--- src/TimeLock.sol
+++ src/TimeLock.sol
@@ -4,9 +4,12 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/governance/TimelockController.sol";
 
+/**
+ * Add a time delay to the execution of proposals.
+ * Intermediary contract between the governance process and the execution of actions.
+ */
 contract TimeLock is TimelockController {
     /**
-     * 
      * @param minDelay initial minimum delay in seconds for operations (how long to wait before executing a proposal)
      * @param proposers accounts to be granted proposer and canceller roles (TODO: update to be anybody)
      * @param executors accounts to be granted executor role (who can execute the proposal after the delay has passed)

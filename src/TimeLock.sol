ad13aa5d0ed9c56c211dfc00b7c7bb00d46f221e
samigabor <samoila.gabor@yahoo.com>
2024-02-25 08:52:16 +0200

    fear: add governor, governor token and governor time lock contracts
    Thank you openzeppelin wizard!

---
+++ src/TimeLock.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract TimeLock is TimelockController {
+    /**
+     * 
+     * @param minDelay initial minimum delay in seconds for operations (how long to wait before executing a proposal)
+     * @param proposers accounts to be granted proposer and canceller roles (TODO: update to be anybody)
+     * @param executors accounts to be granted executor role (who can execute the proposal after the delay has passed)
+     * @param admin optional account to be granted admin role; disable with zero address
+     */
+    constructor(uint256 minDelay, address[] memory proposers, address[] memory executors, address admin)
+        TimelockController(minDelay, proposers, executors, admin)
+    {}
+}
